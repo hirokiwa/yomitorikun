84df0f84831fedd67d18ea42dcf81a0f6418c274
hirokiwa <hirokifsys@gmail.com>
2023-07-06 01:03:08 +0900

    edit top page layout

--- app/page.tsx
+++ app/page.tsx
@@ -35,23 +35,42 @@ export default function Home() {
   }, [history])
   return (
     <main className={styles.main}>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
       <Header />
-      <BodyContentArea>
-        <ReadSection
-          history = {history}
-          setHistory = {setHistory}
-        />
-        <AccessHistory
-          history = {history}
-        />
-        <HowToUse/>
-      </BodyContentArea>
+      <BodyContentAreaWrapper>
+        <SideContentsArea>
+          <AccessHistory
+            history = {history}
+          />
+        </SideContentsArea>
+        <BodyContentArea>
+          <ReadSection
+            history = {history}
+            setHistory = {setHistory}
+          />
+          <HowToUse/>
+        </BodyContentArea>
+      </BodyContentAreaWrapper>
       <Footer/>
     </main>
   )
 }
 
+const BodyContentAreaWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(12, [col-start] 1fr);
+  gap: 1em;
+  width: 100%;
+  max-width: 1250px;
+`
+
 const BodyContentArea = styled.div`
-  width: 90%;
   text-align: center;
+  grid-column: col-start 5 / span 6;
+  grid-row: 1 / 2;
+  `
+const SideContentsArea = styled.div`
+  grid-column: 1 / 4;
+  grid-column: col-start / span 3;
+  grid-row: 1 / 2;
 `
