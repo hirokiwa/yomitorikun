30d56f1c33454c57dac08883427b93be463be33f
hirokiwa <hirokifsys@gmail.com>
2023-07-14 02:43:11 +0900

    test3

--- app/api/site/[url]/route.ts
+++ app/api/site/[url]/route.ts
@@ -1,20 +1,20 @@
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request, params: paramsType) {
-  try {
+  // try {
+    //   const response = await fetch(url);
+    //   const html = await response.text();
+    //   const titleRegex = /<title>(.*?)<\/title>/i;
+    //   const match = html.match(titleRegex);
+    //   const title = match && match[1] ? match[1].trim() : url;
+    
+  //   const faviconRegex = /<link[^>]+rel=["'](?:icon|shortcut icon)["'][^>]+href=["'](.*?)["'][^>]*>/i;
+  //   const faviconMatch = html.match(faviconRegex);
+  //   const faviconUrl = faviconMatch && faviconMatch[1] ? new URL(faviconMatch[1], url).href : undefined;
+  
+  //   return NextResponse.json({ title: title, favicon: faviconUrl });
+  // } catch (error) {
+    // }
     const url = params.params.url
-    const response = await fetch(url);
-    const html = await response.text();
-    const titleRegex = /<title>(.*?)<\/title>/i;
-    const match = html.match(titleRegex);
-    const title = match && match[1] ? match[1].trim() : url;
-
-    const faviconRegex = /<link[^>]+rel=["'](?:icon|shortcut icon)["'][^>]+href=["'](.*?)["'][^>]*>/i;
-    const faviconMatch = html.match(faviconRegex);
-    const faviconUrl = faviconMatch && faviconMatch[1] ? new URL(faviconMatch[1], url).href : undefined;
-
-    return NextResponse.json({ title: title, favicon: faviconUrl });
-  } catch (error) {
-    return NextResponse.json({ title: undefined, favicon: undefined });
-  }
+    return NextResponse.json({ title: url, favicon: url });
 }
