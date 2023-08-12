2dd0be64356a1c5aa573b9e7948cbe793e1438cd
hirokiwa <hirokifsys@gmail.com>
2023-08-12 09:13:28 +0900

    add logo to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,6 +7,10 @@ const Header = () => {
                 href="/"
                 tabIndex={32767}
             >
+                <LogoImage
+                    src="/logo.png"
+                    alt="logo"
+                />
                 <ProductTitle>
                     コピペでQR読み取りくん
                 </ProductTitle>
@@ -26,12 +30,25 @@ const HeaderTop = styled.div`
     padding: 1em;
     `
 
+const LogoImage = styled.img`
+    width: 2em;
+    height: 2em;
+    border: solid 1px #dbdbdb;
+    border-radius: 50%;
+`
+
 const ProductTitle = styled.h1`
+    margin-left: 0.2em;
     font-size: 24px;
     display: inline;
 `
 
 const TitleLink = styled.a`
+    appearance: none;
+    border: none;
+    display: flex;
+    padding: 0;
+    text-decoration: none;
     &:hover{
         color: #cfcfcf;
     }
