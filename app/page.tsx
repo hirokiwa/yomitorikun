60972ef0ecf5b66584434948fcbbd8e113fc953f
hirokiwa <hirokifsys@gmail.com>
2023-10-06 14:45:07 +0900

    replace usehome

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,33 @@ import HowToUse from '@/components/HowToUse';
 import Footer from '@/components/Footer';
 import AccessHistory from '@/components/AccessHistory';
 import ReadSection from '@/components/ReadSection';
-import useHome from '@/hooks/useHome';
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 const Home = () => {
-  const { history, setHistory } = useHome();
   const bodyContentRef = useRef<HTMLDivElement>(null);
+  const [history, setHistory] = useState<urlHistory[]>([]);
+  
+  useEffect(() => {
+    if (!process.env.NEXT_PUBLIC_LOCAL_STORAGE_KEY) {
+      return
+    }
+    const localString = localStorage.getItem(process.env.NEXT_PUBLIC_LOCAL_STORAGE_KEY);
+    if (!localString) {
+      return;
+    }
+    const localData = JSON.parse(localString) as dataFromLocalStrage;
+    setHistory(localData.history)
+  }, [])
+  
+  useEffect(() => {
+    if (!process.env.NEXT_PUBLIC_LOCAL_STORAGE_KEY) {
+      return
+    }
+    const localString = JSON.stringify({
+      history: history
+    })
+    localStorage.setItem(process.env.NEXT_PUBLIC_LOCAL_STORAGE_KEY, localString);
+  }, [history])
 
   return (
     <main className={styles.main}>
